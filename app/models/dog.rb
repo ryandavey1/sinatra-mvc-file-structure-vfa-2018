ce819ae84799d69bbab660337f4d0fc7ee1ad9cf
Ryan Davey <ryancdavey@icloud.com>
2018-06-28 09:55:32 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,7 +1,14 @@
 class Dog
-  configure do
-  	set :name, "app/views"
-  	set :breed, "public"
-  	set :age, "5"
+  
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    DOGS << self
   end
+  # configure do
+  # 	set :name, "app/views"
+  # 	set :breed, "public"
+  # 	set :age, "5"
+  # end
 end
