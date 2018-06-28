0af9a62a33d639b0edb9fb52e05ee4dac38a56e3
Ryan Davey <ryancdavey@icloud.com>
2018-06-28 09:56:24 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,11 +1,17 @@
 class Dog
   
+  attr_accessor :name, :breed, :age
+  dogs = []
   def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
+    dogs << self
   end
-  
+
+  def self.all
+    DOGS
+  end
   # configure do
   # 	set :name, "app/views"
   # 	set :breed, "public"
